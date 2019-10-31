3c1ea332c072fd4c2555a1b9df27a44dc9e26506
jmsridley <jmsridley@gmail.com>
2019-10-31 18:33:14 +0000

    add non-working checkbox

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -3,9 +3,19 @@
 import React from 'react';
 
 function Checkbox(props) {
+    const style = props.enabled ? {backgroundColor:"red"} : {backgroundColor:"blue"};
+
     return(
-        <switch></switch>
+        <div style={{display:"flex",alignItems: "center",margin:"1%"}}>
+            <div className="box" style={style}></div>
+            <h3 style={{textAlign:"left",margin: 0}}>{props.name}</h3>
+            
+        </div>
     );
+}
+
+Checkbox.defaultProps = {
+    enabled: false,
 }
 
 export default Checkbox;
