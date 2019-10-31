437ef4e9228a07b5b6ad6ddcc4e87b8a92ddeca8
Filip Balucha <balucha.filip2@gmail.com>
2019-10-31 18:57:18 +0000

    edit state in App.js to hold multiple checkboxes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "./logo.svg";
 import Button from "./components/Button/Button";
 import Header from "./components/Header/Header";
-import './App.css';
-import TerminalAccess from './components/TerminalAccess/TerminalAccess';
-import Checkbox from './components/Checkbox/Checkbox';
+import "./App.css";
+import TerminalAccess from "./components/TerminalAccess/TerminalAccess";
+import Checkbox from "./components/Checkbox/Checkbox";
 
 class App extends React.Component {
-
   state = {
-    fakeimu_enabled: false,
-  }
+    checkboxes: [
+      {
+        id: "fakeIMUs",
+        enabled: false
+      }
+    ]
+  };
 
-  handleCheckBox = state => {
-    console.log(state);
+  handleCheckBox = checkbox => {
+    const checkboxes = [...this.state.checkboxes];
+    const index = checkboxes.indexOf(checkbox);
+    checkboxes[index].enabled = !checkboxes[index].enabled;
+    console.log(
+      checkboxes[index].id + " is enabled: " + checkboxes[index].enabled
+    );
     this.setState({
-      fakeimu_enabled: !this.state.fakeimu_enabled
-    })
-  }
+      checkboxes
+    });
+  };
   render() {
-  return (
-    <div className="App">
+    return (
+      <div className="App">
         <div className="gui-wrapper">
           <div className="grid-container">
             <Header className="header" title="Internal Debugging GUI"></Header>
             <div className="switches">
               <div className="fake-imu">
-              <Checkbox 
-                name="FAKE IMU" handleClick={() => this.handleCheckBox(this.state.fakeimu_enabled)} enabled={this.state.fakeimu_enabled}>
-              </Checkbox>
+                <Checkbox
+                  name="Fake IMU"
+                  handleClick={this.handleCheckBox}
+                  enabled={this.state.fakeimu_enabled}
+                ></Checkbox>
               </div>
             </div>
             <div className="run-button">
@@ -39,8 +50,8 @@ class App extends React.Component {
             </div>
           </div>
         </div>
-    </div>
-  );
+      </div>
+    );
   }
 }
 
--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,21 +1,20 @@
-
-
-import React from 'react';
+import React from "react";
 
 function Checkbox(props) {
-    const style = props.enabled ? {backgroundColor:"red"} : {backgroundColor:"blue"};
+  const style = props.enabled
+    ? { backgroundColor: "red" }
+    : { backgroundColor: "blue" };
 
-    return(
-        <div style={{display:"flex",alignItems: "center",margin:"1%"}}>
-            <div className="box" style={style}></div>
-            <h3 style={{textAlign:"left",margin: 0}}>{props.name}</h3>
-            
-        </div>
-    );
+  return (
+    <div style={{ display: "flex", alignItems: "center", margin: "1%" }}>
+      <div className="box" style={style}></div>
+      <h3 style={{ textAlign: "left", margin: 0 }}>{props.name}</h3>
+    </div>
+  );
 }
 
 Checkbox.defaultProps = {
-    enabled: false,
-}
+  enabled: false
+};
 
 export default Checkbox;
