702d1d4f48050b023a50ca67e2e0b98254412ea2
Filip Balucha <balucha.filip2@gmail.com>
2019-11-04 01:46:29 +0000

    Implement function that returns a list of enabled flags

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React from "react";
 import logo from "./logo.svg";
 import Button from "./components/Button/Button";
 import Header from "./components/Header/Header";
+
 import "./App.css";
 import TerminalAccess from "./components/TerminalAccess/TerminalAccess";
 import Checkbox from "./components/Checkbox/Checkbox";
+import DebugDropdown from "./components/DebugDropdown/DebugDropdown";
 import config from "./config.json";
 
 class App extends React.Component {
-  state = {
-    checkboxes: config
+  constructor() {
+    super();
+    this.state = {
+      checkboxes: config
+    };
+    this.getEnabledFlags = this.getEnabledFlags.bind(this);
+    this.getCheckboxes = this.getCheckboxes.bind(this);
+  }
+
+  getEnabledFlags = () => {
+    const { checkboxes } = this.state;
+    let enabled_flags = [];
+    Object.keys(checkboxes).forEach(checkbox_name => {
+      if (checkboxes[checkbox_name][0].enabled) {
+        enabled_flags.push(checkboxes[checkbox_name][0].flag);
+      }
+    });
+    return enabled_flags;
   };
 
   handleCheckboxClicked = name => {
@@ -20,19 +38,19 @@ class App extends React.Component {
     });
   };
 
-  getCheckboxes = (checkboxes, handleCheckboxClicked) => {
-    let checkboxList = [];
+  getCheckboxes = () => {
+    const { checkboxes } = this.state;
+    let checkbox_list = [];
     Object.keys(checkboxes).forEach(checkbox_name => {
-      checkboxList.push(
+      checkbox_list.push(
         <Checkbox
-          // name={checkboxes[checkbox_name][0].flag}
           name={checkbox_name}
-          onClick={handleCheckboxClicked}
+          onClick={this.handleCheckboxClicked}
           enabled={checkboxes[checkbox_name][0].enabled}
         />
       );
     });
-    return checkboxList;
+    return checkbox_list;
   };
 
   render() {
@@ -40,14 +58,12 @@ class App extends React.Component {
       <div className="App">
         <div className="gui-wrapper">
           <div className="grid-container">
-            <Header className="header" title="Internal Debugging GUI"></Header>
+            <Header className="header" title="INTERNAL DEBUGGING GUI"></Header>
             <div className="switches">
-              <div>
-                {this.getCheckboxes(
-                  this.state.checkboxes,
-                  this.handleCheckboxClicked
-                )}
-              </div>
+              <div>{this.getCheckboxes()}</div>
+            </div>
+            <div className="debug-dropdown">
+              <DebugDropdown></DebugDropdown>
             </div>
             <div className="run-button">
               <Button name="RUN"></Button>
